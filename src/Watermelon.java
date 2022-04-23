a4432c40a44f7694d9e7994dbb967fe778151b49
aGayar30 <ahmed02amr@gmail.com>
2022-04-23 04:13:12 +0200

    Testing

--- src/Watermelon.java
+++ src/Watermelon.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Watermelon {
     public boolean weight(int w){
-       // if ( w <= 0)
-           // throw new IllegalArgumentException("weight must be positive integer");
+        if ( w <= 0)
+            throw new IllegalArgumentException("weight must be positive integer");
 
         if (w % 2==0 && w != 2)
         return true;
